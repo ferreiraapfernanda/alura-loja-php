4b6f801fcd2c4dd9c8c0f21741b22ee4565af608
Fernanda Aparecida Ferreira <fernanda@amee.com.br>
2017-08-29 13:47:46 -0300

    aula 2: inserindo produtos no banco de dados

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # loja-php
+
+Loja PHP utilizada como exemplo de aprendizagem para o curso de PHP e MySQL I do Alura. [Link para o Curso] (https://cursos.alura.com.br/course/php-mysql-e-fundamentos-da-web/)
--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,11 +1,25 @@
-<?php include("cabecalho.php"); ?>
+<?php include ("cabecalho.php"); ?>
 <?php
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
-?>
-        
-	<p class="alert-success">
-		Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
-	</p>
 
-<?php include("rodape.php"); ?>
+$query = "insert into produtos (nome, preco) values ('{$nome}', '{$preco}')";
+
+// Cria a conexão com o banco
+// Padrão: Endereço, usuário, senha, banco
+$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+
+// Executa a query
+if (mysqli_query($conexao, $query)) {
+	?>
+	<p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php
+
+}
+else {
+	?>
+	<p class="alert-danger">O produto <?= $nome; ?> não foi adicionado</p>
+<?php
+
+}
+include ("rodape.php"); ?>
