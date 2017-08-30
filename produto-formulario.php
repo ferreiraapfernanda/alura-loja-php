144d7a03f2933cf120650e1a0c49d316cbbc5fa4
Fernanda Aparecida Ferreira <fromnanda@users.noreply.github.com>
2017-08-30 09:23:26 -0300

    dispoe campos em tabela

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,11 +1,23 @@
 <?php include("cabecalho.php"); ?>
-
+<html>
         <h1>Formulário de cadastro</h1>
         <form action="adiciona-produto.php">
-            Nome: <input type="text" name="nome" /><br/>
-            Preço: <input type="number" name="preco" /><br/>
-
-            <input type="submit" value="Cadastrar" />
+                <table class="table">
+                        <tr>
+                                <td> Nome </td>
+                                <td> <input type="text" class="form-control" name="nome" /> </td>
+                        </tr>
+                        <tr> 
+                                <td> Preço </td>
+                                <td> <input type="number" class="form-control" name="preco" /> </td>
+                        </tr>
+                        <tr>
+                                <td></td>
+                                <td> <input type="submit" class="btn btn-primary" value="Cadastrar" /> </td>
+                        </tr>
+                        
+                </table>
         </form>
+</html>
 
 <?php include("rodape.php"); ?>
