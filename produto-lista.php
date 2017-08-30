91d01b8623e242b41ba73aab7ea286aa4cefcfb2
Fernanda Aparecida Ferreira <fromnanda@users.noreply.github.com>
2017-08-30 11:34:22 -0300

    Update produto-lista.php

--- produto-lista.php
+++ produto-lista.php
@@ -1,12 +1,23 @@
 <?php include("cabecalho.php"); ?>
 <?php include("conecta.php"); ?>
+<?php include("banco-produto.php"); ?>
 
 <?php
-$resultado = mysqli_query($conexao, "select * from produtos");
-
-while($produto = mysqli_fetch_assoc($resultado)) {
-    echo $produto['nome'] . "<br/>";
-}
+$produtos = listaProdutos($conexao);
 ?>
+<table class="table table-striped table-bordered">
+
+<?php
+foreach($produtos as $produto) :
+?>
+    <tr>
+        <td><?= $produto['nome'] ?></td>
+        <td><?= $produto['preco'] ?></td>
+    </tr>
+
+<?php
+endforeach
+?>
+</table>
 
 <?php include("rodape.php"); ?>
