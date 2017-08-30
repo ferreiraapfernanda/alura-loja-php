c89268c6d69465c7c9bcf7f8448a95756731139e
Fernanda Aparecida Ferreira <fromnanda@users.noreply.github.com>
2017-08-30 09:19:19 -0300

    menu de navegação

--- cabecalho.php
+++ cabecalho.php
@@ -7,6 +7,19 @@
 </head>
 
 <body>
+    
+    <!-- Barra de navegação fixada no topo -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <!-- Tudo fica dentro de um container -->
+        <div class="container">
+            <div class="nav navbar-nav">
+                <li><a href="produto-formulario.php">Adiciona Produto</a></li>
+                    <li><a href="sobre.php">Sobre</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
     <div class="container">
 
         <div class="principal">
