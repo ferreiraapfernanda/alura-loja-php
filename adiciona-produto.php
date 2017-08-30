e423a1cbd41810f4b0cc2f92ab43da08f3509c66
Fernanda Aparecida Ferreira <fromnanda@users.noreply.github.com>
2017-08-30 09:16:13 -0300

    refatoração função inserção
    Separa a função para inserção do produto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,16 +1,21 @@
 <?php include ("cabecalho.php"); ?>
 <?php
+
+function insereProduto ($conexao, $nome, $preco){
+	$query = "insert into produtos (nome, preco) values ('{$nome}', '{$preco}')";
+	$resultado = mysqli_query($conexao, $query);
+	return $resultado;
+}
+
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
-
-$query = "insert into produtos (nome, preco) values ('{$nome}', '{$preco}')";
 
 // Cria a conexão com o banco
 // Padrão: Endereço, usuário, senha, banco
 $conexao = mysqli_connect('localhost', 'root', '', 'loja');
 
 // Executa a query
-if (mysqli_query($conexao, $query)) {
+if (insereProduto($conexao, $nome, $preco)) {
 	?>
 	<p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
 <?php
