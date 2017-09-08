30214e3dd4fb892cffc4ea2c48f0488fc90fb02b
Fernanda Aparecida <ferreira.aparecidaf@gmail.com>
2017-09-08 15:26:18 -0300

    php e mysql II: aula 02

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php include ("cabecalho.php"); ?>
 
+<h1>Bem vindo!</h1>
+
 <?php
+    if( isset($_COOKIE['usuario_logado']) ){
+?>
+    <p class="text-success">Você está logado como <?= $_COOKIE["usuario_logado"] ?></p>
+<?php
+    } else { 
+?>
+
+<h2>Login</h2>
+<form action="login.php" method="post">
+<table class="table"> 
+<tr>
+<td>Email</td> 
+<td><input class="form-control" type="email" name="email"></td>
+</tr>
+<tr>
+<td>Senha</td>
+<td><input class="form-control" type="password" name="senha"></td>
+<tr>
+<td><button type="submit" clas="btn btn-primary">Login</button></td>
+</tr>
+</table>
+</form>
+
+<?php
+    }
+?>
+
+    <?php
     if( isset($_GET["login"]) && $_GET["login"] == true ){
     ?>
     <p class="alert-success">Logado com sucessso!</p>
@@ -13,31 +43,9 @@
     <p class="alert-danger">Usuário ou senha inválida!</p>
     <?php
     }
-
-    if("true" == true){
-        echo "iguais com dois";
-    }
-    if("true" === true){
-        echo "iguais com três";
-    }
     ?>
 
+    
 
-    <h1>Bem vindo!</h1>
-
-    <h2>Login</h2>
-    <form action="login.php" method="post">
-    <table class="table"> 
-    <tr>
-    <td>Email</td> 
-    <td><input class="form-control" type="email" name="email"></td>
-    </tr>
-    <tr>
-    <td>Senha</td>
-    <td><input class="form-control" type="password" name="senha"></td>
-    <tr>
-    <td><button type="submit" clas="btn btn-primary">Login</button></td>
-    </tr>
-    </table>
-    </form>
+    
 <?php include ("rodape.php"); ?>
--- login.php
+++ login.php
@@ -7,6 +7,7 @@ $usuario = buscaUsuario($conexao, $_POST["email"], $_POST["senha"]);
 if($usuario == null){
     header("Location: index.php?login=0");
 }else{
+    setcookie("usuario_logado", $usuario["email"], time() + 60);
     header("Location: index.php?login=1");
 }
 
